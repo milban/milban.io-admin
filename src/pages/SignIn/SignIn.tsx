5a94a38242de23efa2152331429a339f8d82f233
JunHyeock Park <wnsgur6311@gmail.com>
2020-12-15 20:39:24 +0900

    로그인된 사용자의 경우 signin 페이지 접근 제한

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Page from 'src/components/common/Page';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { isAuthState } from 'src/stores/auth';
 import DefaultLayout from 'src/components/Layout/DefaultLayout';
 import useInput from 'src/hooks/common/useInput';
 import AuthModel from 'src/models/auth/AuthModel';
 import { AuthRelayModel } from 'src/models/auth/AuthRelayModel';
+import { useHistory } from 'react-router-dom';
+import { PathName } from 'src/constants/pathName';
 
 const SignIn: React.FC = () => {
+  const history = useHistory();
   const { value: userId, onChange: onChangeUserId } = useInput();
   const { value: password, onChange: onChangePassword } = useInput();
-  const setIsAuth = useSetRecoilState(isAuthState);
+  const [isAuth, setIsAuth] = useRecoilState<boolean>(isAuthState);
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -22,10 +25,17 @@ const SignIn: React.FC = () => {
       const token = await authModel.signIn(userId, password);
       authModel.saveToken(token);
       setIsAuth(true);
+      history.replace(PathName.HOME);
     } catch (e) {
       console.log(e);
     }
   };
+
+  useEffect(() => {
+    if (isAuth) {
+      history.replace(PathName.HOME);
+    }
+  }, [isAuth]);
 
   return (
     <DefaultLayout>
